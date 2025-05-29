5b158a41b1d9cb8fa50830a6edb2fd0e507e13c8
Justas Andrijauskas <justas.andrijauskas@novian.lt>
2025-05-29 12:22:38 +0300

    Remove null values in JSON constructed out of PaymentResponseDto

--- src/main/java/com/example/paymentservice/dto/PaymentResponseDto.java
+++ src/main/java/com/example/paymentservice/dto/PaymentResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.paymentservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,6 +8,7 @@ import java.math.BigDecimal;
 
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PaymentResponseDto {
     private Long id;
     private Boolean isCanceled;
