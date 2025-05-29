9c149a5c0ea366bf1893e0f53684a08b47976aac
Justas Andrijauskas <justas.andrijauskas@novian.lt>
2025-05-29 11:21:35 +0300

    Add table annotations to Payment entity for clarity

--- src/main/java/com/example/paymentservice/entity/Payment.java
+++ src/main/java/com/example/paymentservice/entity/Payment.java
@@ -2,11 +2,13 @@ package com.example.paymentservice.entity;
 
 import com.example.paymentservice.types.CurrencyType;
 import com.example.paymentservice.types.PaymentType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.persistence.Version;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,23 +26,37 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "PAYMENTS")
 public class Payment {
     @Id
     @GeneratedValue
+    @Column(name = "ID", nullable = false)
     private Long id;
+    @Column(name = "CREATED_AT", nullable = false)
+    private LocalDateTime createdAt;
+    @Column(name = "AMOUNT", nullable = false)
     private BigDecimal amount;
     @Enumerated(EnumType.STRING)
+    @Column(name = "CURRENCY", nullable = false)
     private CurrencyType currency;
+    @Column(name = "DEBTOR_IBAN", nullable = false)
     private String debtorIban;
+    @Column(name = "CREDITOR_IBAN", nullable = false)
     private String creditorIban;
     @Enumerated(EnumType.STRING)
+    @Column(name = "TYPE", nullable = false)
     private PaymentType type;
+    @Column(name = "DETAILS")
     private String details;
+    @Column(name = "CREDITOR_BANK_BIC")
     private String creditorBankBic;
+    @Column(name = "IS_CANCELED", nullable = false)
     private Boolean isCanceled;
-    private LocalDateTime createdAt;
+    @Column(name = "CANCELLATION_FEE")
     private BigDecimal cancellationFee;
+    @Column(name = "NOTIFICATION_SUCCESS")
     private Boolean notificationSuccess;
     @Version
+    @Column(name = "VERSION")
     private Long version;
 }
